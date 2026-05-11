da0a5641bf26f1f8aef80d63e260ddbbb52ddcb2
shalshcode08 <somushrestha448@gmail.com>
2026-05-11 18:07:06 +0530

    fix: use resolvedTheme and conditional icon rendering in ThemeToggle to prevent hydration mismatches

--- components/appComponents/ThemeToggle.tsx
+++ components/appComponents/ThemeToggle.tsx
@@ -5,18 +5,20 @@ import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
+
   return (
-    <div>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      >
-        <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </Button>
-    </div>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? (
+        <SunIcon className="h-4 w-4" />
+      ) : (
+        <MoonIcon className="h-4 w-4" />
+      )}
+    </Button>
   );
 };
 
